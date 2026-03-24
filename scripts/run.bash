0bc88c944f1376fbff652da49a7cb167bc65edde
Einar Lopez Altamirano <elopezal@mahti-login11.mahti.csc.fi>
2026-03-24 17:46:38 +0200

    Working bash script

--- scripts/run.bash
+++ scripts/run.bash
@@ -2,14 +2,14 @@
 #SBATCH --account=project_2018357
 #SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=0:30:00
+#SBATCH --cpus-per-task=32
+#SBATCH --time=1:00:00
 #SBATCH --gres=gpu:a100:1,nvme:20
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export DATASET_DIR=$LOCAL_SCRATCH
 export BATCH_SIZE=256
-export ROOT_PATH = "/projappl/project_2018357/model_distillation_medical_images"
+export ROOT_PATH="/projappl/project_2018357/model_distillation_medical_images"
 
 unzip -q /scratch/project_2018357/data/chexpert.zip -d $LOCAL_SCRATCH
 
