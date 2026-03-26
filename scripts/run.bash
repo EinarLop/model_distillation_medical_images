c7a5357eaacf10b1a2fdd3e1db6576dbfd4f35c0
Einar Lopez Altamirano <elopezal@mahti-login11.mahti.csc.fi>
2026-03-26 13:28:07 +0200

    Successful 01 Experiment

--- scripts/run.bash
+++ scripts/run.bash
@@ -12,7 +12,7 @@ export DATASET_DIR=$LOCAL_SCRATCH
 export BATCH_SIZE=32
 export ROOT_PATH="/projappl/project_2018357/model_distillation_medical_images"
 export TRAIN_SUBSET_SIZE=2000
-export VAL_SUBSET_SIZE=400
+export VAL_SUBSET_SIZE=200
 export CHECKPOINT_NAME="checkpoint_reduced_gpu_run2403.pth"
 
 unzip -q /scratch/project_2018357/data/chexpert.zip -d $LOCAL_SCRATCH
